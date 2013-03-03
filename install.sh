d8f4d28f8e4e5a7f697f29d66f49bfd0d6c60dab
Jeff Siegel <jdsiegel@gmail.com>
2013-03-02 17:06:19 -0700

    convert from jr to kb
    BYE JAMIE!

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 set -e
-[ -n "$JR_DEBUG" ] && set -x
+[ -n "$kb_DEBUG" ] && set -x
 
 install_bash() {
-  jr-chef-apply <<RECIPE
+  kb-chef-apply <<RECIPE
     package "bash"
 RECIPE
 }
 
 install_shunit2() {
-  local shunit2_path="$(jr-vendorpath shunit2)"
+  local shunit2_path="$(kb-vendorpath shunit2)"
   local tar="/tmp/shunit2-$$.tgz"
   local extracted="${tar%%.tgz}"
   local url="http://shunit2.googlecode.com/files/shunit2-2.1.6.tgz"
 
-  ( jr-download "$url" "$tar" && \
+  ( kb-download "$url" "$tar" && \
     mkdir -p "$extracted" && \
     (cd "$extracted" ; gunzip -c "$tar" | tar xf - --strip-components=1) && \
     mkdir -p "$shunit2_path" && \
--- provision.sh
+++ provision.sh
@@ -1,16 +1,16 @@
 apt-get install -y curl
 
-/opt/chef/embedded/bin/ruby -e "$(curl -fsSkL raw.github.com/jamie-ci/jr/go)"
+/opt/chef/embedded/bin/ruby -e "$(curl -fsSkL https://raw.github.com/opscode/kb/go)"
 
-mkdir -p /opt/jr/suites/shunit2
-cat > /opt/jr/suites/shunit2/dummy_test.sh <<SCRIPT
+mkdir -p /opt/kb/suites/shunit2
+cat > /opt/kb/suites/shunit2/dummy_test.sh <<SCRIPT
 test_pass()
 {
   assertEquals "squeeee" "squeeee"
 }
 SCRIPT
 
-mkdir -p /opt/jr/plugins
-[ -e /opt/jr/plugins/shunit2 ] || ln -s /vagrant /opt/jr/plugins/shunit2
-/opt/jr/bin/jr sh-shell /opt/jr/plugins/shunit2/install.sh
+mkdir -p /opt/kb/plugins
+[ -e /opt/kb/plugins/shunit2 ] || ln -s /vagrant /opt/kb/plugins/shunit2
+/opt/kb/bin/kb sh-shell /opt/kb/plugins/shunit2/install.sh
 
